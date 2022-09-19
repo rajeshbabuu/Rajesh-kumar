968e1d43ea95b7c0b988abc59c9649fd0dbd39d7
Rajesh <rajeshbabu656656.com>
2022-09-19 18:46:02 +0530

    Hello GitHub

---
+++ shelldemo/demoshell.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+numberValue=1
+stringValue="TPF26"
+echo $numberValue
+echo $stringValue
+echo $"*************"
+i=0
+for((i=0;i<10;i++))
+do 
+     echo $i
+done
+
+echo "***************"
+#while loop syntex
+q=10
+while [$q !=0]
+	echo$q
+	((q--)) 
+done
+
+echo"**********"
+#read -p "Enter the name " empName
+echo "XYZ work's in Eng dep"
