f77006fef76276c1a1245f4d3b01401d496a0530
Rajesh <rajeshbabu656656.com>
2022-09-21 10:37:00 +0530

    to find all prime no upto n

--- reviewprogram/primeno.sh
+++ reviewprogram/primeno.sh
@@ -18,32 +18,14 @@ flag=0
 break
 fi
 j=$(( $j+1 ))
-
-echo "Enter a limit"
-read limit
-echo "prime no upto $limit are :"
-echo "1"
-i=2
-while [ $i -le $limit ]
-do
-flag=1
-j=2
-while [ $j -lt $i ]
-do
-	rem=$(( $i % $j ))
-if [ $rem -eq 0 ]
-then
-flag=0
-break
-fi
-j=$(( $j+1 ))
 done
-if  [ $flag -eq 1 ]
+if [ $flag -eq 1 ]
 then
 echo "$i"
 fi
-	i=$(( $i+1 ))
+i=$(( $i+1 ))
 done
+
 
 
 
